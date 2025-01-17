6183fc838018506a8958dce3a2b8536a6a7fd5fe
zafchiel <bialakropla@proton.me>
2025-01-17 16:56:05 +0100

    ignore streamelements bot

--- src/index.ts
+++ src/index.ts
@@ -8,25 +8,25 @@ config();
 
 // Configure logger
 const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.json()
-  ),
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: "grypsiarze.log" }),
-  ],
+	level: "info",
+	format: winston.format.combine(
+		winston.format.timestamp(),
+		winston.format.json(),
+	),
+	transports: [
+		new winston.transports.Console(),
+		new winston.transports.File({ filename: "grypsiarze.log" }),
+	],
 });
 
 // Configure TMI client
 const client = new tmi.Client({
-  options: { debug: true },
-  identity: {
-    username: process.env.TWITCH_USERNAME,
-    password: process.env.TWITCH_OAUTH_TOKEN,
-  },
-  channels: [process.env.CHANNEL_NAME as string],
+	options: { debug: true },
+	identity: {
+		username: process.env.TWITCH_USERNAME,
+		password: process.env.TWITCH_OAUTH_TOKEN,
+	},
+	channels: [process.env.CHANNEL_NAME as string],
 });
 
 // Connect to Twitch
@@ -34,32 +34,33 @@ client.connect().catch(console.error);
 
 // Store user messages
 client.on("message", async (channel, userstate, message, self) => {
-  if (self) return;
+	if (self) return;
+	if (userstate.username === "streamelements") return;
 
-  try {
-    await insertMessage(userstate.username ?? "", message);
-  } catch (error) {
-    console.log(error);
-    logger.error(error);
-  }
+	try {
+		await insertMessage(userstate.username ?? "", message);
+	} catch (error) {
+		console.log(error);
+		logger.error(error);
+	}
 });
 
 // Listen for timeout events
 client.on("timeout", async (channel, username, reason, duration, userstate) => {
-  try {
-    await insertZbrodniarze("timeout", channel, username, duration);
-  } catch (error) {
-    logger.error(error);
-  }
+	try {
+		await insertZbrodniarze("timeout", channel, username, duration);
+	} catch (error) {
+		logger.error(error);
+	}
 });
 
 // Listen for ban events
 client.on("ban", async (channel, username, reason, userstate) => {
-  try {
-    await insertZbrodniarze("ban", channel, username, 0);
-  } catch (error) {
-    logger.error(error);
-  }
+	try {
+		await insertZbrodniarze("ban", channel, username, 0);
+	} catch (error) {
+		logger.error(error);
+	}
 });
 
 // Error handling
