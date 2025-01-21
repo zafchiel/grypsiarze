949b09355fc9816c103c984c7b52db58f239b9a4
zafchiel <bialakropla@proton.me>
2025-01-21 20:02:50 +0100

    create indexes

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,5 @@
 import { sql } from "drizzle-orm";
-import { text, sqliteTable, integer } from "drizzle-orm/sqlite-core";
+import { text, sqliteTable, integer, index } from "drizzle-orm/sqlite-core";
 
 export const zbrodniarzeTable = sqliteTable("zbrodniarze", {
   id: integer("id").primaryKey({ autoIncrement: true }),
@@ -10,9 +10,18 @@ export const zbrodniarzeTable = sqliteTable("zbrodniarze", {
   duration: integer("duration").notNull(),
 });
 
-export const messagesTable = sqliteTable("messages", {
-  id: integer("id").primaryKey({ autoIncrement: true }),
-  username: text("username").notNull(),
-  message: text("message").notNull(),
-  timestamp: text("timestamp").default(sql`CURRENT_TIMESTAMP`),
-});
+export const messagesTable = sqliteTable(
+  "messages",
+  {
+    id: integer("id").primaryKey({ autoIncrement: true }),
+    username: text("username").notNull(),
+    message: text("message").notNull(),
+    timestamp: text("timestamp").default(sql`CURRENT_TIMESTAMP`),
+  },
+  (table) => {
+    return {
+      usernameIdx: index("username_idx").on(table.username),
+      timestampIdx: index("timestamp_idx").on(table.timestamp),
+    };
+  }
+);
