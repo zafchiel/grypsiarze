347ea363e618987743dbbe95a1e2a32fd4ea6e1c
zafchiel <bialakropla@proton.me>
2025-04-17 19:10:52 +0200

    disable message clean up for now

--- src/index.js
+++ src/index.js
@@ -52,7 +52,7 @@ client.on("timeout", async (channel, username, reason, duration, userstate) => {
   try {
     await insertZbrodniarze("timeout", channel, username, duration);
     await incrementDailyStat("timeout");
-    await deleteMessagesExceptLastFiveBeforeEachZbrodnia(username);
+    // await deleteMessagesExceptLastFiveBeforeEachZbrodnia(username);
   } catch (error) {
     logger.error(`Error handling timeout for ${username}:`, error);
   }
@@ -63,18 +63,18 @@ client.on("ban", async (channel, username, reason, userstate) => {
   try {
     await insertZbrodniarze("ban", channel, username, 0);
     await incrementDailyStat("ban");
-    await deleteMessagesExceptLastFiveBeforeEachZbrodnia(username);
+    // await deleteMessagesExceptLastFiveBeforeEachZbrodnia(username);
   } catch (error) {
     logger.error(`Error handling ban for ${username}:`, error);
   }
 });
 
 // Configure cron job to run every 24 hours
-cron.schedule("0 0 * * *", async () => {
-  try {
-    await deleteOldMessagesExceptZbrodniarze(24); // 24 hours = 1 day
-    logger.info("Cleaned up old messages");
-  } catch (error) {
-    logger.error("Error cleaning up old messages:", error);
-  }
-});
+// cron.schedule("0 0 * * *", async () => {
+//   try {
+//     await deleteOldMessagesExceptZbrodniarze(24); // 24 hours = 1 day
+//     logger.info("Cleaned up old messages");
+//   } catch (error) {
+//     logger.error("Error cleaning up old messages:", error);
+//   }
+// });
